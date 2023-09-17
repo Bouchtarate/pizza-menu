fbb28ebc160d9d700d2d91cab522b35ff1fe2ca5
Bouchtarate <hamza.bouchtarate@gmail.com>
2023-09-17 12:54:15 +0100

    add images css file and data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Pizza Menu App !</h1>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return <h3>We starting soon</h3>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
