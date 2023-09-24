c896304fba6c4fcb286501631941ee0e9cb4b7ae
Bouchtarate <hamza.bouchtarate@gmail.com>
2023-09-24 13:28:31 +0100

    add data to the pizza component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
+import "./data.js";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
   return (
     <div className="container">
@@ -25,12 +71,17 @@ const Menu = () => {
       <main className="menu">
         <h2>OUR MENU</h2>
         <Description />
-        <Pizza
-          photoPath="pizzas/focaccia.jpg"
-          pizzaName="pizza focaccia"
-          ingredients="Tomato, Mozzarella, Spinach, And ricotta cheese."
-          price={22.4}
-        />
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza
+              key={pizza.name}
+              photoPath={pizza.photoName}
+              pizzaName={pizza.name}
+              ingredients={pizza.ingredients}
+              price={pizza.price}
+            />
+          ))}
+        </ul>
       </main>
     </div>
   );
@@ -44,21 +95,22 @@ const Description = () => {
   );
 };
 
-const Pizza = ({ photoPath, pizzaName, ingredients, price }) => {
+const Pizza = ({ photoPath, pizzaName, ingredients, price, soldOut }) => {
   return (
-    <div className="pizza">
+    <li className="pizza">
       <img src={photoPath} alt={pizzaName} />
       <div>
         <h3>{pizzaName}</h3>
         <p>{ingredients}</p>
         <span>{price} $</span>
       </div>
-    </div>
+    </li>
   );
 };
 
 const Footer = () => {
   const hours = new Date().getHours();
+
   const openHours = 12;
   const closeHours = 22;
   let closeMsg;
@@ -68,7 +120,13 @@ const Footer = () => {
     closeMsg = "closed";
   }
 
-  return <footer className="footer">{closeMsg}</footer>;
+  return (
+    <footer className="footer">
+      <p className="order">{closeMsg}</p>
+
+      <button className="btn">Order Now</button>
+    </footer>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
